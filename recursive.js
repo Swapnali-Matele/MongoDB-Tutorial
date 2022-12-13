53664c3b2153c4f0fc0e56aa88357ad60e8388c9
Swapnali-Matele <swapnali.matele@wohlig.com>
2022-12-13 10:21:35 +0530

    last

---
+++ recursive.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const {MongoClient} = require('mongodb');
+const client = new MongoClient('mongodb+srv://root:123@cluster0.gq2rjja.mongodb.net/?retryWrites=true&w=majority')
+const test = require('assert');
+
+
+app.use(express.json());
+var database ;
+
+app.get ( '/', (req, res) => {
+    res.send('Welcome to MongoDB API'); 
+})  
+
+app.get('/api/finance', (req, res) => {
+    
+    const {page =1, limit =25} = req.query
+    database.collection('finance_quotes').find().limit(25).skip(25).toArray((err, result)=>{
+        if (err) throw err;
+        console.log(err)
+        console.log(result.length)
+        console.log(result)
+
+        
+        res.send(result);
+    })
+
+    
+})
+
+app.listen(3000, () => {
+//connection url 
+client.connect( ).then((result) => {
+    database = result.db('finance')
+    console.log("Connection to database Successful")
+    
+}).catch((err) => {
+    if (err) throw err;
+})
+})
